8d4859923352fbbe8591329f0a5526fda5411f9d
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-09-04 09:47:38 -0400

    Definición de código

---
+++ src/main/java/GestorInventario.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class GestorInventario {
+
+    public static void main(String[] args) {
+        Object[][] matrizProductos = crearMatrizProductos();
+        definirProducto("Bloxy Cola", 1, 4, matrizProductos);
+
+
+    }
+
+    public static Object[][] crearMatrizProductos() { // Esta función crea la matriz de productos
+        Object[][] productos = new Object[10][3];
+        return productos;
+    }
+
+    }
+
+    public static void definirProducto(String nombreProducto, int idProducto, int cantidad, Object[][] productos) {
+        for (int fila = 0; fila < productos.length; fila++) {
+            for (int columna = 0; columna < 3; columna++) {
+                if (productos[fila][columna] != null) {
+                    productos[fila][0] = idProducto;
+                    productos[fila][1] = nombreProducto;
+                    productos[fila][2] = cantidad;
+                }
+            } // Este método define un producto que será añadido a la matriz principal
+        }
+    }
+
+}
